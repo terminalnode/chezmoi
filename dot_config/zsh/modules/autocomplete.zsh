f321e4fc5cfa66967157923a6a15cd07422b15a4
Alexander Rundberg <alex@gwai.se>
2025-06-13 00:24:52 +0200

    [zsh] add hcloud autocomplete

--- dot_config/zsh/modules/autocomplete.zsh
+++ dot_config/zsh/modules/autocomplete.zsh
@@ -14,7 +14,13 @@ if [[ -x $(which scw) ]]; then
   eval "$(scw autocomplete script shell=zsh)"
 fi
 
+# HCloud CLI autocomplete
+if [[ -x $(which hcloud) ]]; then
+  eval "$(hcloud completion zsh)"
+fi
+
 # Terraform autocomplete
 if [[ -x $(which terraform) ]]; then
   complete -o nospace -C /usr/bin/terraform terraform
 fi
+
