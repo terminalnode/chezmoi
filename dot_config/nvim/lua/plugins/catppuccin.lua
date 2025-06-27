0871db91d8f9858f74c67f25cee53608825cf0ca
Alexander Rundberg <alex@gwai.se>
2025-06-27 16:06:58 +0200

    [nvim] enable catppuccin integrations

--- dot_config/nvim/lua/plugins/catppuccin.lua
+++ dot_config/nvim/lua/plugins/catppuccin.lua
@@ -5,6 +5,17 @@ return {
     priority = 1000,
     config = function()
       vim.cmd([[colorscheme catppuccin-latte]])
+      require("catppuccin").setup({
+        flavour = "latte",
+        dim_inactive = { enabled = false },
+        integrations = {
+          blink_cmp = true,
+          mason = true,
+          nvim_surround = true,
+          snacks = true,
+          telescope = { enabled = true },
+        },
+      })
     end,
   },
 }
