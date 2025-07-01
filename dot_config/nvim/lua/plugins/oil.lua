7628745b0abe7bd052cab1327b0eb2e51fa3519b
Alexander Rundberg <alex@gwai.se>
2025-07-01 09:35:29 +0200

    [nvim] add permissions to oil

--- dot_config/nvim/lua/plugins/oil.lua
+++ dot_config/nvim/lua/plugins/oil.lua
@@ -1,22 +1,29 @@
 return {
-  {
-    "stevearc/oil.nvim",
-    keys = {
-      { "-", "<Cmd>Oil<CR>", desc = "Run oil" },
-    },
-    ---@module "oil"
-    ---@type oil.SetupOpts
-    opts = {
-      win_options = {
-        signcolumn = "yes:2",
-      },
-    },
-    dependencies = { "mini.icons" }, -- Can also use nvim-web-devicons
-  },
+	{
+		"stevearc/oil.nvim",
+		dependencies = { "mini.icons" }, -- Can also use nvim-web-devicons
+		keys = {
+			{ "-", "<Cmd>Oil<CR>", desc = "Run oil" },
+		},
 
-  {
-    "refractalize/oil-git-status.nvim",
-    dependencies = { "stevearc/oil.nvim" },
-    config = true,
-  },
+		---@module "oil"
+		---@type oil.SetupOpts
+		opts = {
+			watch_for_changes = true,
+			columns = {
+				-- "mtime", "size",
+				"icon",
+				"permissions",
+			},
+			win_options = {
+				signcolumn = "yes:2",
+			},
+		},
+	},
+
+	{
+		"refractalize/oil-git-status.nvim",
+		dependencies = { "stevearc/oil.nvim" },
+		config = true,
+	},
 }
